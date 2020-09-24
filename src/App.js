392feb6176af3563014e8b981bd14b3b2c095df6
Miguel Jimenez <mjimenez08@gmail.com>
2020-09-23 20:36:52 -0400

    routes working and connected

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import { Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Main from './components/Main';
+import { Link } from 'react-router-dom';
 
-function App() {
-  return (
-  
-<div className="demo-big-content">
+class App extends Component {
+  render() {
+    return (
+      <div className="demo-big-content">
     <Layout>
-        <Header title="Title" scroll>
+        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">MyPortfolio</Link>} scroll>
             <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
+                <Link to="/resume">Resume</Link>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
             </Navigation>
         </Header>
-        <Drawer title="Title">
+        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">MyPortfolio</Link>}>
             <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
+              <Link to="/resume">Resume</Link>
+              <Link to="/aboutme">About Me</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="/contact">Contact</Link>
             </Navigation>
         </Drawer>
         <Content>
             <div className="page-content" />
+            <Main/>
         </Content>
     </Layout>
 </div>
-  );
+
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Project.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+
+class Projects extends Component {
+    render() {
+        return (
+            <div><h1>Project Page!</h1></div>
+        )
+    }
+}
+
+export default Projects;
