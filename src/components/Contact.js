f412b3d1b200969d26cd580d2e1a7796d8fb3f4c
Miguel Jimenez <mjimenez08@gmail.com>
2020-09-23 23:37:08 -0400

    test

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 
 class Contact extends Component {
     render() {
         return(
-            <div><h1>Contact Page</h1></div>
+           <div className="contact-body">
+               <Grid className="contact-grid">
+
+                   <Cell col={6}>text here</Cell>
+                   <Cell col={6}>text here</Cell>
+                   
+
+
+
+               </Grid>
+           </div>
         )
     }
 }
